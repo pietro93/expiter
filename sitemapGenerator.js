1116449651923e3bce826c25c77b4cc6660c1a81
pietro93 <xpietro93@hotmail.com>
2022-09-20 15:59:46 +0200

    sitemap

---
+++ sitemapGenerator.js
@@ -0,0 +1,56 @@
+import { createServer } from 'http';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+var dataset;
+var siteMap= '<?xml version="1.0" encoding="UTF-8"?>'+
+'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+
+'   <url>'+
+'   <loc>https://expiter.com/</loc>'+
+'   <priority>1</priority>'+
+'   </url>'+
+'   <url>'+
+'   <loc>https://expiter.com/index.html</loc>'+
+'   <priority>1</priority>'+
+'   </url>'+
+'   <url>'+
+'   <loc>https://expiter.com/about.html</loc>'+
+'   <priority>.5</priority>'+
+'   </url>'+
+'   <url>'+
+'   <loc>https://expiter.com/resources.html</loc>'+
+'   <priority>.5</priority>'+
+'   </url>'
+
+createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+fetch('https://expiter.com/dataset.json', {method:"Get"})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        dataset=data;  
+        for (let i = 0; i < 107; i++){
+            let province = dataset[i];
+            console.log("adding /province/" + dataset[i].Name +".html to sitemap")
+            siteMap=siteMap.concat('<url>'+
+            '<loc>https://expiter.com/province/'+dataset[i].Name+'.html</loc>'+
+            '<priority>.8</priority>'+
+            '</url>')
+        }
+
+        siteMap=siteMap.concat('</urlset>')
+        
+        var fileName = './autositemap.xml';
+            
+            fs.writeFile(fileName, siteMap, function (err, file) {
+                if (err) throw err;
+                else console.log(fileName+' Saved!');
+            });
+        })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
