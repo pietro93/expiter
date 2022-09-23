104915b3b4c77276f1149031a155b936b1dcbdc3
pietro93 <xpietro93@hotmail.com>
2022-09-23 16:56:33 +0200

    .

--- indexGeneratorSSR.js
+++ indexGeneratorSSR.js
@@ -459,7 +459,7 @@ function setNavBar($){
     '</div>'+
     '<ul class="menu-items">'+
         '<li><a href="/">Home</a></li>'+
-        '<li><a href="/resources.html">Resources</a></li>'+
+        '<li><a href="/resources">Resources</a></li>'+
         '<li><a href="/app#About">About</a></li>'+
         '<li><a href="https://forms.gle/WiivbZg8336TmeUPA" target="_blank">Take Survey</a></li>'+
         '</ul>'+
--- pageGeneratorSSR.js
+++ pageGeneratorSSR.js
@@ -274,7 +274,7 @@ function populateFacts(){
         '</div>'+
         '<ul class="menu-items">'+
             '<li><a href="/">Home</a></li>'+
-            '<li><a href="/resources.html">Resources</a></li>'+
+            '<li><a href="/resources">Resources</a></li>'+
             '<li><a href="/app#About">About</a></li>'+
             '<li><a href="https://forms.gle/WiivbZg8336TmeUPA" target="_blank">Take Survey</a></li>'+
             '</ul>'+
