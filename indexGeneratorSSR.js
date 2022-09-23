f51668b76645429a14f1b7427416c0570fbbd23a
pietro93 <xpietro93@hotmail.com>
2022-09-23 13:07:51 +0200

    .

--- indexGeneratorSSR.js
+++ indexGeneratorSSR.js
@@ -404,9 +404,9 @@ function appendData($,data) {
      
 
     for (let i = 0; i < data.length; i++) {
-        let card = "<card id='"+data[i].Name+"'class='"+(data[i].Name=="Aosta"?"Aosta Valley":data[i].Region)+" paracard' "+
-         "title='"+data[i].Name+", "+(data[i].Name=="Aosta"?"Aosta Valley":data[i].Region)+"'"+
-         ">";
+        let card = '<card id="'+data[i].Name+'"class="'+(data[i].Name=="Aosta"?"Aosta Valley":data[i].Region)+' paracard" '+
+         'title="'+data[i].Name+', '+(data[i].Name=="Aosta"?"Aosta Valley":data[i].Region)+'"'+
+         '>';
          
         let col = "<div class='column'>";
 
