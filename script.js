d112c8b8a9d5ba9b36b3bcdb4bec094db74ba1cb
pietro93 <xpietro93@hotmail.com>
2022-09-28 15:25:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ fetch('../dataset.json', {method:"Get"})
         dataset = data;
         selection=dataset.slice(0, 107);
         if (!document.getElementById("navbar").innerHTML) setNavBar();
+        let theme = localStorage.getItem('data-theme'); // Retrieve saved them from local storage
+        if (theme ==='dark') changeThemeToLight()
         //setTimeout(function(){
         //if (document.title=="Italy Expats and Nomads")
         //filterDataByRegion("All")
