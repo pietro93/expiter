404ca6b320576fc3b1d688a5d3016c29bd3e5eef
pietro93 <xpietro93@hotmail.com>
2022-09-29 19:00:52 +0200

    .

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -82,7 +82,7 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
             "Piemonte","Puglia","Sardegna","Sicilia","Toscana","Trentino-Alto Adige","Umbria","Valle d&#39;Aosta","Veneto"];
         var region_filters = ["All","North","South","Center"].concat(regions)
         region_filters = region_filters.concat(
-            combine(regions,2).concat(combine(regions,3))//.concat(combine(regions,4))
+            combine(regions,2)//.concat(combine(regions,3))//.concat(combine(regions,4))
         )
         console.log(region_filters)
 
@@ -91,8 +91,8 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
         "Name","CostOfLiving","Population","Veg-friendly","Family-friendly","Density","Climate","Safety","Nightlife","Education","Crime"].sort()
 
         var filter_filters=filters.concat(
-            combine(filters,2).concat(combine(filters,3))//.concat(combine(filters,4))
-            //.concat(combine(filters,5)).concat(combine(filters,6)).concat(combine(filters,7))
+            combine(filters,2).concat(combine(filters,3)).concat(combine(filters,4))
+            .concat(combine(filters,5)).concat(combine(filters,6)).concat(combine(filters,7))
             //.concat(combine(filters,8)).concat(combine(filters,9))
         )
         filter_filters= filter_filters.filter(
