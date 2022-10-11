48c767077722aa7f45fede111d25c4d96fdd4f2b
pietro93 <xpietro93@hotmail.com>
2022-10-11 23:15:46 +0200

    -

--- townPageGeneratorItaly.js
+++ townPageGeneratorItaly.js
@@ -33,7 +33,7 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
         let comuniSiteMap='<?xml version="1.0" encoding="UTF-8"?> '+'\n'+
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> '+'\n';
        
-        for (let i = 56; i < 70; i++){
+        for (let i = 0; i < 30; i++){
             let province = dataset[i];
        
             if (fs.existsSync('temp/'+province.Name+'-comuni.json')){
