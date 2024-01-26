f1cb838adf468c23ce43631b9cd6616320a7a31c
pietro93 <xpietro93@hotmail.com>
2024-01-26 23:49:04 +0100

    -

--- townPageGeneratorItaly.js
+++ townPageGeneratorItaly.js
@@ -35,7 +35,7 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
         let comuniSiteMap='<?xml version="1.0" encoding="UTF-8"?> '+'\n'+
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> '+'\n';
        
-        for (let i = 34; i < 46; i++){
+        for (let i = 95; i < 107; i++){
             let province = dataset[i];
             let sidebar=pb.setSideBarIT(province)
 
