422a24860d5ca8392ecc7dc2939585fc2275b815
Pietro <xpietro93@hotmail.com>
2024-02-04 13:13:33 +0100

    -

--- townPageGenerator.js
+++ townPageGenerator.js
@@ -34,7 +34,7 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
         let comuniSiteMap='<?xml version="1.0" encoding="UTF-8"?> '+'\n'+
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> '+'\n';
 
-        for (let i = 99; i < 107; i++){
+        for (let i = 96; i < 107; i++){
             let province = dataset[i];
             let sidebar=pb.setSideBar(province)
        
