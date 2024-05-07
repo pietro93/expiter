75a83550c4f4ed5a88a9e5c0c062855bfff53197
Pietro <xpietro93@hotmail.com>
2024-05-07 17:19:29 +0700

    -

--- CaSGenerator.js
+++ CaSGenerator.js
@@ -88,7 +88,7 @@ fetch('https://expiter.com/dataset.json', { method: 'Get' })
     }
 
       // Create .htaccess file in the directory
-let htaccessContent = `RewriteEngine on\nRewriteRule ^$ ${dirName}.html [L]\nRewriteRule ^([a-zA-Z0-9-]+)/$ $1.html [L]`;
+let htaccessContent = `RewriteEngine on\nRewriteRule ^([a-zA-Z0-9-]+)/$ $1.html [L]\nRewriteRule ^$ ${dirName}.html [L]\n`;
 fs.writeFileSync(path.join(dir, '.htaccess'), htaccessContent);
 
 
