9aa95db3eb9c0eb6171ce1bf1afe6c3279792e03
Pietro <xpietro93@hotmail.com>
2024-06-19 21:02:24 +0700

    -

--- QoLgeneratorItaly.js
+++ QoLgeneratorItaly.js
@@ -124,7 +124,7 @@ fetch('https://expiter.com/dataset.json', {method:"Get"})
         '<section id="overview"><h2>Vivere a '+province.Name+'</h2><span id="intro"></span></section>'+
         '<section id="generalinfo"><h2>Informazioni utili su '+province.Name+'</h2><span id="info"></span></section>'+
         '<section id="costofliving"><h2>Costo della vita a '+province.Name+'</h2><span id="CoL"></span></section>'+
-        '<section id="povertyandsafety"><h2>È '+province.Name+' sicura?</h2><span id="safety"></span></section>'+
+        '<section id="povertyandsafety"><h2>'+province.Name+' è pericolosa?</h2><span id="safety"></span></section>'+
         '<section id="prosandcons"><h2>Pro e contro della vita a '+province.Name+'</h2><span id="ProsAndCons"></span></section>'+
         '<section id="faqs"><h2>FAQ</h2><span id="faqs"></span></section>'+
         '<section id="Discover"><h2>Scopri '+province.Name+'</h2><span id="promo"></span></section>'+
